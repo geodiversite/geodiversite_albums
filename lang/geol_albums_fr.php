44acec5327b20bc55e2504da8dfe11402496cdec
Salvatore <salvatore@arscenic.org>
2020-03-09 01:27:10 +0100

    [Salvatore] [source: geol_albums] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source: geol_albums] Mise a jour du bilan depuis https://trad.spip.net

--- lang/geol_albums_fr.php
+++ lang/geol_albums_fr.php
@@ -1,19 +1,21 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://github.com/geodiversite/geodiversite_albums/trunk/lang
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'bouton_lier' => 'Ajouter',
-'bouton_ouvrir' => 'Autoriser',
-'bouton_quitter' => 'Quitter',
-'bouton_suprrimer' => 'Supprimer',
+	// B
+	'bouton_lier' => 'Ajouter',
+	'bouton_ouvrir' => 'Autoriser',
+	'bouton_quitter' => 'Quitter',
+	'bouton_suprrimer' => 'Supprimer',
 
-
-'explication_ouvrir' => 'Vous pouvez ouvrir cette collection à la coopération afin d\'autoriser les autres auteurs à y ajouter des médias.',
-'explication_suivi' => 'Suivre les contributions de la collection via le fil RSS. Exporter la carte de ses contributions vers un autre outil de cartographie.',
-'explication_supprimer' => 'Supprimer cette collection.',
-'explication_supprimer_confirm' => 'Êtes-vous sûr de vouloir supprimer cette collection ? (le contenu de la collection ne sera pas supprimé)',
-
-
+	// E
+	'explication_ouvrir' => 'Vous pouvez ouvrir cette collection à la coopération afin d’autoriser les autres auteurs à y ajouter des médias.',
+	'explication_suivi' => 'Suivre les contributions de la collection via le fil RSS. Exporter la carte de ses contributions vers un autre outil de cartographie.',
+	'explication_supprimer' => 'Supprimer cette collection.',
+	'explication_supprimer_confirm' => 'Êtes-vous sûr de vouloir supprimer cette collection ? (le contenu de la collection ne sera pas supprimé)'
 );

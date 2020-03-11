5990cfba8515a45dd455ad714eb6012e2aca895f
filipaun <filipaun@gmail.com>
2020-03-11 01:27:19 +0100

    [Salvatore] [source:lang/ geol_albums] Export depuis https://trad.spip.net de la langue pt_br

---
+++ lang/geol_albums_pt_br.php
@@ -0,0 +1,23 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/geol_albums-geodiversite_albums?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_lier' => 'Acrescentar',
+	'bouton_ouvrir' => 'Autorizar',
+	'bouton_quitter' => 'Sair',
+	'bouton_suprrimer' => 'Apagar',
+
+	// E
+	'explication_ouvrir' => 'Você pode tornar esta coleção colaborativa e permitir que outros autores incluam outras mídias.',
+	'explication_suivi' => 'Seguir as contribuições da coleção via o fluxo RSS. Exportar o mapa de suas contribuições para outra ferramenta de cartografia.',
+	'explication_supprimer' => 'Apagar esta coleção.',
+	'explication_supprimer_confirm' => 'Tem certeza de que deseja apagar esta coleção? (o conteúdo da coleção não será apagado)'
+);

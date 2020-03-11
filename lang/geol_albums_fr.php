108f93290f1af74e9ecc114086edf113e20664ea
Salvatore <salvatore@rezo.net>
2020-03-11 01:27:19 +0100

    [Salvatore] [source:lang/ geol_albums] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ geol_albums] Mise a jour du bilan depuis https://trad.spip.net

--- lang/geol_albums_fr.php
+++ lang/geol_albums_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans https://github.com/geodiversite/geodiversite_albums/trunk/lang
+// Fichier source, a modifier dans https://github.com/geodiversite/geodiversite_albums.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }

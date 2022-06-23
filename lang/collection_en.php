80667c1c7acdca89fd4a9e5dc76b78ca17662d03
brunob <bruno@eliaz.fr>
2022-06-23 14:10:15 +0200

    copier les items de langue des collections

---
+++ lang/collection_en.php
@@ -0,0 +1,148 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/collection?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_collection' => 'Add this collection',
+
+	// B
+	'bouton_diogene_statut_poubelle_normal' => 'Put your collection in the dustbin',
+	'bouton_diogene_statut_poubelle_normal_pas_auteur' => 'Put this collection in the dustbin',
+	'bouton_dissocier' => 'Unlink',
+	'bouton_fermer_collection' => 'Make this collection personal',
+	'bouton_media_associer' => 'Link to a collection',
+	'bouton_ouvrir_collection' => 'Make this collection collaborative',
+	'bouton_quitter_collection' => 'No longer collaborate to this collection',
+	'bouton_rejoindre_collection' => 'Collaborate to this collection',
+
+	// C
+	'collections_titre' => 'Collections',
+
+	// D
+	'description_page_collections' => 'All the website’s collections.',
+	'diogene_champ_collection' => 'Add to collection',
+
+	// E
+	'erreur_association_collection' => 'The ombination with the collection failed.',
+	'erreur_autorisation_statut_modifier' => 'You are not allowed to edit this collection.',
+	'erreur_collection_non_existante' => 'This collection doesn’t exist.',
+	'erreur_media_document' => 'Warning. The type of this document does not match the type of the collection..',
+	'erreur_selectionner_un_media' => 'Select at least one media',
+	'explication_genre' => 'Which kind of medias can be linked to that collection?',
+	'explication_genre_defaut' => 'Default value for the types of media that can be attached (useful when fast creation).',
+	'explication_medias_ajouter' => 'Media below are not linked to the collection and may correspond to its kind. Select the ones you want to add.',
+	'explication_medias_modifier' => 'Media below are associated with the collection. To dissociate some, select them and press the delete button.',
+	'explication_medias_modifier_drag' => 'You can reorder them via drag and drop.',
+	'explication_participer' => 'Click the button below to participate to this collection.',
+	'explication_participer_non' => 'Click the button below to no longer participate to this collection.',
+	'explication_type' => 'Which is the use of the collection?',
+	'explication_type_defaut' => 'Default use of the collection (useful when fast creation).',
+
+	// G
+	'genre_mixed' => 'Mix (all types)',
+	'genre_musique' => 'Sounds (only sound type: music...)',
+	'genre_photo' => 'Pictures (only image type: photos, icons ...)',
+	'genre_video' => 'Videos (only video type)',
+
+	// I
+	'icone_creer_collection' => 'Create a collection',
+	'icone_modifier_collection' => 'Manage this collection',
+	'info_1_collection' => 'A collection',
+	'info_aucun_collection' => 'No collection',
+	'info_collaborateurs' => 'Contributors:',
+	'info_collection_creation' => 'Collection created on <abbr title="@date_abbr@">@date@</abbr> <span class="auteurs">by @auteurs@</span>.',
+	'info_collection_maj' => 'Updated: ',
+	'info_collections_auteur' => 'This author’s  collections',
+	'info_media_dans_collection' => 'In the collection: ',
+	'info_media_dans_collections' => 'In the @nb@ collections: ',
+	'info_medias_1' => 'A media',
+	'info_medias_aucun' => 'No media',
+	'info_medias_nb' => '@nb@ medias',
+	'info_nb_collections' => '@nb@ collections',
+	'info_non_autorise_creer_collection' => 'The author "@id_auteur@" is not allowed to create a collection.',
+	'info_non_autorise_modifier_collection' => 'The author "@id_auteur@" is not allowed to edit the collection @id@.',
+	'info_rang_collection' => 'Rank in the collection',
+
+	// L
+	'label_choix_collection' => 'Link to the collection',
+	'label_collections_ajouter' => 'Add to the collection',
+	'label_collections_retirer' => 'Remove from collection',
+	'label_date' => 'Date',
+	'label_date_point' => 'Date:',
+	'label_descriptif' => 'Description',
+	'label_descriptif_point' => 'Description:',
+	'label_genre' => 'Types of medias',
+	'label_genre_defaut' => 'Default types of medias',
+	'label_genre_point' => 'Types of medias:',
+	'label_id_admin' => 'Administrator of the collection',
+	'label_id_admin_point' => 'Administrator of the collection:',
+	'label_mediabox' => 'MediaBox',
+	'label_mediabox_long' => 'Do not use MediaBox to display the form which links a media to a collection.',
+	'label_titre' => 'Title',
+	'label_titre_creer_rapide' => 'Title of the collection',
+	'label_titre_point' => 'Title:',
+	'label_type' => 'Use',
+	'label_type_defaut' => 'Default use',
+	'label_type_point' => 'Use:',
+	'legende_collection' => 'Collections',
+	'legende_medias_collection_ajouter' => 'Add medias',
+	'legende_medias_collection_modifier' => 'Edit the list of media',
+	'lien_editer_collection' => 'Edit this collection',
+	'lien_toutes_collections' => 'All collections',
+
+	// M
+	'message_aucune_collection_critere' => 'No collection matches your criteria.', # Utile pour des squelettes
+	'message_aucune_collection_publiee' => 'No collection is currently published.', # Utile pour des squelettes
+	'message_collection_aucun_media_publie' => 'No media which you are the author is available',
+	'message_collection_media_utiliser_autres_auteurs' => 'You can link media of other authors using form for adding on each page of media.',
+	'message_collection_media_utiliser_interface' => 'Publish one or more media via the dedicated interface in order to add them to this collection.',
+	'message_collection_reorganisee' => 'The collection has been reorganized.',
+	'message_collection_vide' => 'This collection is empty.',
+	'message_nombre_ajoute' => '1 linked media.',
+	'message_nombre_ajoutes' => '@nb@ linked medias.',
+	'message_nombre_dissocie' => '1 unlinked media.',
+	'message_nombre_dissocies' => '@nb@ unlinked medias.',
+
+	// O
+	'optgroup_collections_autres' => 'The other collections (collaborative)',
+	'optgroup_collections_votre' => 'Your collections',
+	'option_erreur_genre' => 'The type does not match the media: @genre@',
+
+	// P
+	'publier_une_collection' => 'A collection',
+	'publier_une_collection_desc' => 'Create a media collection.',
+
+	// R
+	'retirer_lien_collection' => 'Unlink this collection',
+	'retirer_tous_liens_collections' => 'Unlink all collections',
+
+	// T
+	'tab_associer_collection' => 'Medias of the collection',
+	'tab_editer_collection' => 'Edition of the collection',
+	'texte_ajouter_collection' => 'Add a collection',
+	'texte_changer_statut_collection' => 'This collection is:',
+	'texte_creer_associer_collection' => 'Create and link a collection',
+	'texte_statut_publie' => 'published online',
+	'title_voir_media' => 'Display the media: @titre@',
+	'titre_collection' => 'Collection',
+	'titre_collections' => 'Collections',
+	'titre_collections_rubrique' => 'Sections of the section',
+	'titre_creer_collection_rapide' => 'Create a collection',
+	'titre_langue_collection' => 'Language of this collection',
+	'titre_logo_collection' => 'Logo de this collection',
+	'titre_media' => 'Media #@rang@: @titre@',
+	'titre_modifier_collection' => 'Edit the collection: @titre@',
+	'titre_modifier_collection_diogene' => 'Manage this collection',
+	'titre_page_collections' => 'Collections',
+	'titre_page_configurer_collections' => 'Configure the collections',
+	'titre_participer' => 'Participate',
+	'type_coop' => 'Cooperative',
+	'type_perso' => 'Personnal'
+);

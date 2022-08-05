1b61aa1ad26d1c7f3294d8097706c1a3aa1d5c93
brunob <bruno@eliaz.fr>
2022-08-05 16:07:47 +0200

    fix: retrait de variables indéfinies passées à `formulaires_editer_objet_traiter()`

--- formulaires/creer_collection_rapide.php
+++ formulaires/creer_collection_rapide.php
@@ -31,7 +31,7 @@ function formulaires_creer_collection_rapide_verifier_dist($retour = '') {
 }
 
 function formulaires_creer_collection_rapide_traiter_dist($retour = '') {
-	$res = formulaires_editer_objet_traiter('collection', 'oui', '', $lier_trad, $retour, $config_fonc, $row, $hidden);
+	$res = formulaires_editer_objet_traiter('collection', 'oui', '', '', $retour);
 	if (intval($res['id_collection']) && is_numeric($res['id_collection'])) {
 		include_spip('action/editer_objet');
 		$err = objet_instituer('collection', $res['id_collection'], ['statut' => 'publie']);

08a23ccb8f3e82e21fe8d4cbde9045be858ef705
brunob <bruno@eliaz.fr>
2023-07-10 21:34:26 +0200

    fix: version 4.2, compat SPIP 4.2 & selecteurgenerique v2

--- geol_albums_pipelines.php
+++ geol_albums_pipelines.php
@@ -5,21 +5,6 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 /**
- * Insertion dans le pipelines insert_head (SPIP)
- * Insérer les js du séleceteur générique s'ils ne sont pas déjà là
- *
- * @param string $flux
- * 	Le contenu textuel de la balise #INSERT_HEAD
- * @return string
- * 	Le contenu modifié
- */
-function geol_albums_insert_head($flux) {
-	include_spip('selecteurgenerique_fonctions');
-	$flux .= selecteurgenerique_verifier_js($flux);
-	return $flux;
-}
-
-/**
  * Insertion des css du plugin dans les pages publiques
  *
  * @param $flux

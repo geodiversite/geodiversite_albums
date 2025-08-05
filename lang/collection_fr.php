516c9ed17762f65072f49a5f8f8977637a6d3b8e
brunob <bruno@eliaz.fr>
2025-08-05 21:24:46 +0200

    chore: retrait de champs inutilisés sur les collections
    Fix: #6

--- lang/collection_fr.php
+++ lang/collection_fr.php
@@ -31,23 +31,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'erreur_association_collection' => 'L’association à la collection a échoué.',
 	'erreur_autorisation_statut_modifier' => 'Vous n’êtes pas autorisé à modifier cette collection.',
 	'erreur_collection_non_existante' => 'Cette collection n’existe pas.',
-	'erreur_media_document' => 'Attention. Le type de ce document ne correspond pas au genre de la collection.',
 	'erreur_selectionner_un_media' => 'Veuillez sélectionner au moins un média.',
-	'explication_genre' => 'Quels types de médias peuvent être attachés à cette collection ?',
-	'explication_genre_defaut' => 'Valeur par défaut pour le types de médias pouvant être attaché (utile lors de la création rapide).',
-	'explication_medias_ajouter' => 'Les médias ci-dessous ne sont pas associés à la collection et peuvent correspondre à son genre. Sélectionnez ceux que vous souhaitez ajouter.',
 	'explication_medias_modifier' => 'Les médias ci-dessous sont associés à la collection. Pour en dissocier certains, sélectionnez les et appuyez sur le bouton de suppression.',
 	'explication_medias_modifier_drag' => 'Vous pouvez les réordonner via glisser / déposer.',
 	'explication_participer' => 'Cliquez sur le bouton ci-dessous pour participer à cette collection.',
 	'explication_participer_non' => 'Cliquez sur le bouton ci-dessous pour ne plus participer à cette collection.',
 	'explication_type' => 'Quel est l’usage de la collection ?',
 	'explication_type_defaut' => 'Valeur par défaut de l’usage de la collection (utile lors de la création rapide).',
-
-	// G
-	'genre_mixed' => 'Mix (tous les types)',
-	'genre_musique' => 'Sons (uniquement de type sonore : musique...)',
-	'genre_photo' => 'Images (uniquement de type image : photos, icones...)',
-	'genre_video' => 'Vidéos (uniquement de type vidéo)',
 
 	// I
 	'icone_creer_collection' => 'Créer une collection',
@@ -76,9 +66,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_date_point' => 'Date :',
 	'label_descriptif' => 'Descriptif',
 	'label_descriptif_point' => 'Descriptif :',
-	'label_genre' => 'Types de médias',
-	'label_genre_defaut' => 'Types de médias par défaut',
-	'label_genre_point' => 'Types de médias :',
 	'label_id_admin' => 'Administrateur de la collection',
 	'label_id_admin_point' => 'Administrateur de la collection :',
 	'label_mediabox' => 'MediaBox',
@@ -98,7 +85,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// M
 	'message_aucune_collection_critere' => 'Aucune collection ne correspond à vos critères.', # Utile pour des squelettes
 	'message_aucune_collection_publiee' => 'Aucune collection n’est actuellement publiée.', # Utile pour des squelettes
-	'message_collection_aucun_media_publie' => 'Aucun média dont vous êtes l’auteur et correspondant au genre de la collection n’est disponible.',
 	'message_collection_media_utiliser_autres_auteurs' => 'Vous pouvez lier les médias d’autres auteurs en utilisant le formulaire d’ajout sur chaque page de média.',
 	'message_collection_media_utiliser_interface' => 'Publiez un ou plusieurs médias via l’interface dédiée afin de pouvoir les ajouter à cette collection.',
 	'message_collection_reorganisee' => 'La collection a été réorganisée.',
@@ -111,7 +97,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// O
 	'optgroup_collections_autres' => 'Les autres collections (collaboratives)',
 	'optgroup_collections_votre' => 'Vos collections',
-	'option_erreur_genre' => 'Le type ne correspond pas au média : @genre@',
 
 	// P
 	'publier_une_collection' => 'Une collection',

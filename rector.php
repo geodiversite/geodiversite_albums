59f6c20cf677cd5474528ecf74f36e9303114a2c
brunob <bruno@eliaz.fr>
2026-08-12 16:49:37 +0200

    chore: up composer.json & switch vers qa-tools & suivre la conf des plugins-dist pour rector

--- rector.php
+++ rector.php
@@ -3,12 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use SpipLeague\Component\Rector\Set\SpipSetList;
 
 return RectorConfig::configure()
 	->withPaths([__DIR__])
 	->withRootFiles()
-	->withSets([SpipSetList::SPIP_41, LevelSetList::UP_TO_PHP_74])
-	->withSkip([__DIR__ . '/lang', __DIR__ . '/vendor'])
-;
+	->withPhpSets(php74: true)
+	->withSkip([__DIR__ . '/lang', __DIR__ . '/vendor']);
